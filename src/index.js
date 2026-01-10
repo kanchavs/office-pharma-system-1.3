288aece2c9f5a6acf0c35341a5fb6c3164adf830
kanchavs <kanchavee.1999@gmail.com>
2026-01-10 22:14:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // ตรวจสอบว่าชื่อไฟล์ App.js สะกดตรงกัน
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
